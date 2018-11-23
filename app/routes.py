f5648b047bf9bd7b1aa32fa462d0b52bfb6ed02a
Eli Sandine <eli.sandine@gmail.com>
2018-11-23 10:46:46 -0800

    Added templates to page.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Eli'}
+    posts = [
+        {
+            'author': {'username': 'Eli'},
+            'body': 'A very rainy day in Corvallis.'
+        },
+        {
+            'author': {'username': 'Kurupt'},
+            'body': 'Oh, hey. What a warm chair'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
