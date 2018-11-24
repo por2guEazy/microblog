f38ff24796d736d6ca701195e99c0659652c4206
Eli Sandine <eli.sandine@gmail.com>
2018-11-24 14:40:52 -0800

    Finished chapter 5, added user registration and login/logout capabilities

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,9 +17,8 @@ class User(db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_passwor(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
 
 
 class Post(db.Model):
@@ -29,3 +29,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
