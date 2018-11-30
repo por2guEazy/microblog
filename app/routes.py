283471e80996bddaafaaa3738cb17fbc4d8555cb
Eli Sandine <eli.sandine@gmail.com>
2018-11-30 10:39:09 -0800

    Added password reset page, password reset methods, json token verification

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
-    PostForm
+    PostForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 
 
 @app.before_request
@@ -156,3 +157,19 @@ def unfollow(username):
     db.session.commit()
     flash(f"You are no longer following {username}")
     return redirect(url_for('user', username=username))
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for instructions to reset password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form
+                           )
