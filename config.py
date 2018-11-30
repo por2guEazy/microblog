56d540389bfd5d401160948a030bc5f0deb7d545
Eli Sandine <eli.sandine@gmail.com>
2018-11-30 09:56:48 -0800

    Finished chapter 9, added pagination for posts on home and user pages

--- config.py
+++ config.py
@@ -15,3 +15,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['elisandine@yahoo.com']
+
+    POSTS_PER_PAGE = 3
+    # POSTS_PER_PAGE = 25
