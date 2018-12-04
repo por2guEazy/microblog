dc48341986bd565f83b9ab8b9e45349315030e9e
Eli Sandine <eli.sandine@gmail.com>
2018-12-04 09:11:29 -0800

    Finished chapter 12, also added flask Babel for languages

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
@@ -20,6 +21,8 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -48,6 +51,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_language.best_match(app.config['LANGUAGES'])
 
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -16,5 +16,6 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['elisandine@yahoo.com']
 
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 5
+    LANGUAGES = ['en', 'es']
     # POSTS_PER_PAGE = 25
