378dcf029b8ee2ef6d3b27256948ff635716f836
Eli Sandine <eli.sandine@gmail.com>
2018-12-12 14:25:36 -0800

    Added Azure Translator Key

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['elisandine@yahoo.com']
-
     # For Flask-Babel
     LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
