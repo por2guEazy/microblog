c8f7b30dd20399d6f1ea0057b345f58cd8a0abc5
Eli Sandine <eli.sandine@gmail.com>
2018-12-13 13:41:37 -0800

    Finished ch15, restructured the application. Added requirements.txt

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from time import time
-import jwt
-from flask_login import UserMixin
-from app import app, db, login
 from hashlib import md5
+from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import db, login
 
 
 followers = db.Table(
@@ -64,17 +65,22 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
-    def verify_reset_passowrd_token(token):
+    def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        # Added Exception to get rid of bare except clause
-        except Exception:
+        except:
             return
         return User.query.get(id)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -86,8 +92,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
