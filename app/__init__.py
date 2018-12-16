4f01c32b22086151647013e5e2f6780795801c0b
Eli Sandine <eli.sandine@gmail.com>
2018-12-15 16:42:15 -0800

    Added elasticsearch

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from elasticsearch import Elasticsearch
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from config import Config
@@ -34,6 +35,9 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELSTICSEARCH_URL'] else None
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
