e6129dbdb368bbcd6fc050add4743e4693eefc9c
Eli Sandine <eli.sandine@gmail.com>
2018-12-22 14:50:47 -0800

    Finishing up ch16, adding search capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from elasticsearch import Elasticsearch
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from elasticsearch import Elasticsearch
 from config import Config
 
 
@@ -35,7 +35,6 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
-
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
--- app/search.py
+++ app/search.py
@@ -14,7 +14,7 @@ def add_to_index(index, model):
 def remove_from_index(index, model):
     if not current_app.elasticsearch:
         return
-    current_app.elasticsearch.delete(index=index, doc_type=index, id=id.model)
+    current_app.elasticsearch.delete(index=index, doc_type=index, id=model.id)
 
 
 def query_index(index, query, page, per_page):
@@ -23,6 +23,6 @@ def query_index(index, query, page, per_page):
     search = current_app.elasticsearch.search(
         index=index, doc_type=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
-              'from': (page-1) * per_page, 'size': per_page})
+              'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']
